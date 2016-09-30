4554447c13ef4dcf9b64a147613e79daed7b8af4
lpw <heisedebaise@hotmail.com>
2016-09-30 14:18:39 +0800

    增加获取组件版本号功能。

--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/status/StatusImpl.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/status/StatusImpl.java
@@ -1,12 +1,18 @@
 package org.lpw.tephra.ctrl.status;
 
 import net.sf.json.JSONObject;
+import org.lpw.tephra.bean.BeanFactory;
 import org.lpw.tephra.bean.ContextRefreshedListener;
 import org.lpw.tephra.util.Logger;
 import org.lpw.tephra.util.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * @author lpw
@@ -20,6 +26,7 @@ public class StatusImpl implements Status, ContextRefreshedListener {
     @Value("${tephra.ctrl.status.uri:/tephra/ctrl/status}")
     protected String uri;
     protected boolean enable;
+    protected JSONObject version;
 
     @Override
     public boolean isStatus(String uri) {
@@ -31,6 +38,7 @@ public class StatusImpl implements Status, ContextRefreshedListener {
         JSONObject json = new JSONObject();
         json.put("concurrent", counter);
         json.put("timestamp", System.currentTimeMillis());
+        json.put("version", version);
 
         return json;
     }
@@ -45,5 +53,77 @@ public class StatusImpl implements Status, ContextRefreshedListener {
         enable = !validator.isEmpty(uri);
         if (logger.isInfoEnable())
             logger.info("设置服务状态启动状态：{}", enable);
+
+        version();
+    }
+
+    protected void version() {
+        Set<String> set = new HashSet<>();
+        for (String beanName : BeanFactory.getBeanNames()) {
+            Object bean = BeanFactory.getBean(beanName);
+            if (bean != null)
+                set.add(bean.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        }
+
+        Map<String, Set<String>> map = new HashMap<>();
+        set.forEach(path -> {
+            path = path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'));
+            if (path.startsWith("spring"))
+                return;
+
+            int indexOf = indexOfNumber(path);
+            if (indexOf == -1)
+                return;
+
+            putToMap(map, path.substring(0, indexOf - 1), path.substring(indexOf));
+        });
+        setVersion(map);
+    }
+
+    protected int indexOfNumber(String string) {
+        for (int i = 0, length = string.length(); i < length; i++) {
+            char ch = string.charAt(i);
+            if (ch >= '0' && ch <= '9')
+                return i;
+        }
+
+        return -1;
+    }
+
+    protected void putToMap(Map<String, Set<String>> map, String name, String version) {
+        StringBuilder sb = new StringBuilder();
+        for (String string : name.split("-")) {
+            sb.append('-').append(string);
+            Set<String> set = map.get(sb.toString());
+            if (set == null)
+                set = new HashSet<>();
+            set.add(version);
+            map.put(sb.toString(), set);
+        }
+    }
+
+    protected void setVersion(Map<String, Set<String>> map) {
+        Set<String> set = new HashSet<>();
+        map.forEach((key, value) -> {
+            if (value.size() > 1) {
+                set.add(key);
+
+                return;
+            }
+        });
+        set.forEach(key -> map.remove(key));
+
+        Map<String, String> versions = new HashMap<>();
+        map.forEach((key, value) -> versions.put(key, value.iterator().next()));
+
+        set.clear();
+        versions.forEach((key, value) -> versions.forEach((k, v) -> {
+            if (key.contains(k) && !key.equals(k) && value.equals(v))
+                set.add(key);
+        }));
+        set.forEach(key -> versions.remove(key));
+
+        version = new JSONObject();
+        versions.forEach((key, value) -> version.put(key.substring(1), value));
     }
 }
