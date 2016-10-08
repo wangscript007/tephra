01e0cb58eb36b4059caae67fe7a2f5af9a237246
lpw <heisedebaise@hotmail.com>
2016-10-08 17:08:15 +0800

    简化验证规则资源key设置。

--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/Validate.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/Validate.java
@@ -1,6 +1,10 @@
 package org.lpw.tephra.ctrl.validate;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 验证规则。
