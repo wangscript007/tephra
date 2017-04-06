7daf0da8a7b9a3b1aa752b7a09d3b13fdc8be1aa
lpw <heisedebaise@hotmail.com>
2017-04-06 17:41:34 +0800

    完善经纬度验证器。

--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/LatitudeValidatorImpl.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/LatitudeValidatorImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 public class LatitudeValidatorImpl extends ValidatorSupport {
     @Override
     public boolean validate(ValidateWrapper validate, String parameter) {
-        return validator.isMatchRegex("^-?\\d{1,2}\\.\\d+$", parameter) && Math.abs(converter.toDouble(parameter)) <= 90;
+        return validator.isMatchRegex("^-?\\d{1,2}\\.?\\d*$", parameter) && Math.abs(converter.toDouble(parameter)) <= 90;
     }
 
     @Override
--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/LongitudeValidatorImpl.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/LongitudeValidatorImpl.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Controller;
 public class LongitudeValidatorImpl extends ValidatorSupport {
     @Override
     public boolean validate(ValidateWrapper validate, String parameter) {
-        return validator.isMatchRegex("^\\d{1,3}\\.\\d+$", parameter) && Math.abs(converter.toDouble(parameter) - 90) <= 90;
+        return validator.isMatchRegex("^\\d{1,3}\\.?\\d*$", parameter) && Math.abs(converter.toDouble(parameter) - 90) <= 90;
     }
 
     @Override
     protected String getDefaultFailureMessageKey() {
-        return Validators.PREFIX + "illegal-latitude";
+        return Validators.PREFIX + "illegal-longitude";
     }
 }
