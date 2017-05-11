a7191f39d42a5c748c1adbb0a76cbd97a90d16da
lpw <heisedebaise@hotmail.com>
2017-05-11 23:38:42 +0800

    增加签名验证器开关。

--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/SignValidatorImpl.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/SignValidatorImpl.java
@@ -2,6 +2,7 @@ package org.lpw.tephra.ctrl.validate;
 
 import org.lpw.tephra.ctrl.TrustfulIp;
 import org.lpw.tephra.ctrl.context.Header;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
@@ -15,10 +16,12 @@ public class SignValidatorImpl extends ValidatorSupport {
     private Header header;
     @Inject
     private TrustfulIp trustfulIp;
+    @Value("${" + Validators.SIGN + ".enable:true}")
+    private boolean enable;
 
     @Override
     public boolean validate(ValidateWrapper validate, String parameter) {
-        return trustfulIp.contains(header.getIp()) || request.checkSign();
+        return !enable || trustfulIp.contains(header.getIp()) || request.checkSign();
     }
 
     @Override
