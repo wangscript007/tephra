555b3ec990d464822c3f74eaf9f8c29d9c62006b
lpw <heisedebaise@hotmail.com>
2017-08-29 17:25:07 +0800

    修正单元测试BUG。

--- tephra-script/src/test/java/org/lpw/tephra/script/ArgumentsTest.java
+++ tephra-script/src/test/java/org/lpw/tephra/script/ArgumentsTest.java
@@ -3,6 +3,8 @@ package org.lpw.tephra.script;
 import org.junit.Assert;
 import org.junit.Test;
 import org.lpw.tephra.test.CoreTestSupport;
+import org.lpw.tephra.util.Thread;
+import org.lpw.tephra.util.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -11,12 +13,16 @@ import javax.inject.Inject;
  */
 public class ArgumentsTest extends CoreTestSupport {
     @Inject
+    private Thread thread;
+    @Inject
     private Engine engine;
     @Inject
     private Arguments arguments;
 
     @Test
     public void execute() throws Exception {
+        thread.sleep(2, TimeUnit.Second);
+
         arguments.set("arg", "arg from java");
         Assert.assertEquals("arg from java", engine.execute("arguments_execute"));
         Assert.assertEquals("arg from javascript", arguments.get("arg"));
@@ -24,6 +30,8 @@ public class ArgumentsTest extends CoreTestSupport {
 
     @Test
     public void all() throws Exception {
+        thread.sleep(2, TimeUnit.Second);
+
         arguments.all().clear();
         int sum = 0;
         for (int i = 0; i < 10; i++) {
--- tephra-script/src/test/java/org/lpw/tephra/script/EngineTest.java
+++ tephra-script/src/test/java/org/lpw/tephra/script/EngineTest.java
@@ -3,6 +3,8 @@ package org.lpw.tephra.script;
 import org.junit.Assert;
 import org.junit.Test;
 import org.lpw.tephra.test.CoreTestSupport;
+import org.lpw.tephra.util.Thread;
+import org.lpw.tephra.util.TimeUnit;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -12,10 +14,13 @@ import java.io.IOException;
  */
 public class EngineTest extends CoreTestSupport {
     @Inject
+    private Thread thread;
+    @Inject
     private Engine engine;
 
     @Test
     public void execute() throws IOException {
+        thread.sleep(2, TimeUnit.Second);
         Assert.assertEquals("message from javascript", engine.execute("engine_execute"));
     }
 }
