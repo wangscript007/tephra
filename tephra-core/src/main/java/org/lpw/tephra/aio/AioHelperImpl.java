5b1098ce725fec62a33ffdfeaf8917cff79e3928
lpw <heisedebaise@hotmail.com>
2017-10-27 10:44:56 +0800

    优化AIO功能。

--- tephra-core/src/main/java/org/lpw/tephra/aio/AioHelperImpl.java
+++ tephra-core/src/main/java/org/lpw/tephra/aio/AioHelperImpl.java
@@ -1,23 +1,32 @@
 package org.lpw.tephra.aio;
 
+import org.lpw.tephra.scheduler.MinuteJob;
 import org.lpw.tephra.util.Logger;
+import org.lpw.tephra.util.Thread;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author lpw
  */
 @Component("tephra.aio.helper")
-public class AioHelperImpl implements AioHelper {
+public class AioHelperImpl implements AioHelper, MinuteJob {
+    @Inject
+    private Thread thread;
     @Inject
     private Logger logger;
     private Map<String, AsynchronousSocketChannel> map = new ConcurrentHashMap<>();
+    private Map<String, ExecutorService> services = new ConcurrentHashMap<>();
 
     @Override
     public String put(AsynchronousSocketChannel socketChannel) {
@@ -37,7 +46,8 @@ public class AioHelperImpl implements AioHelper {
         if (!map.containsKey(sessionId))
             return;
 
-        map.get(sessionId).write(ByteBuffer.wrap(message));
+        services.computeIfAbsent(sessionId, sid -> Executors.newFixedThreadPool(1))
+                .submit(() -> map.get(sessionId).write(ByteBuffer.wrap(message)));
     }
 
     @Override
@@ -47,8 +57,21 @@ public class AioHelperImpl implements AioHelper {
 
         try {
             map.remove(sessionId).close();
+            services.remove(sessionId);
         } catch (IOException e) {
             logger.warn(e, "关闭AIO Socket Channel[{}]时发生异常！", sessionId);
         }
+    }
+
+    @Override
+    public void executeMinuteJob() {
+        Set<String> set = new HashSet<>();
+        map.forEach((sessionId, socketChannel) -> {
+            if (socketChannel.isOpen())
+                return;
+
+            set.add(sessionId);
+        });
+        set.forEach(this::close);
     }
 }
