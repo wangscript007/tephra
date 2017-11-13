6298a557919df84e57caa79a8692e62c6fb540a5
lpw <heisedebaise@hotmail.com>
2017-11-13 22:21:41 +0800

    开发批量SQL执行功能。

--- tephra-dao/src/main/java/org/lpw/tephra/dao/jdbc/JdbcSupport.java
+++ tephra-dao/src/main/java/org/lpw/tephra/dao/jdbc/JdbcSupport.java
@@ -25,6 +25,8 @@ abstract class JdbcSupport<T extends PreparedStatement> implements Jdbc {
     Logger logger;
     @Inject
     private Connection connection;
+    @Inject
+    private BatchUpdate batchUpdate;
 
     SqlTable query(ResultSet rs) throws SQLException {
         SqlTable sqlTable = BeanFactory.getBean(SqlTable.class);
@@ -52,6 +54,9 @@ abstract class JdbcSupport<T extends PreparedStatement> implements Jdbc {
 
     @Override
     public int update(String dataSource, String sql, Object[] args) {
+        if (batchUpdate.collect(dataSource, sql, args))
+            return -1;
+
         try {
             long time = System.currentTimeMillis();
             T pstmt = newPreparedStatement(dataSource, Mode.Write, sql);
