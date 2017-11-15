a017862ad5d43f0a83e9b7c2d6de3028647a6e5f
lpw <heisedebaise@hotmail.com>
2017-11-15 21:41:20 +0800

    完善批量SQL执行功能。

--- tephra-dao/src/main/java/org/lpw/tephra/dao/jdbc/BatchUpdateImpl.java
+++ tephra-dao/src/main/java/org/lpw/tephra/dao/jdbc/BatchUpdateImpl.java
@@ -46,13 +46,10 @@ public class BatchUpdateImpl implements BatchUpdate {
         if (tlDataSource.get() == null)
             return false;
 
-        System.out.println("yyyyyyyyy11:"+tlIgnore.get());
-        System.out.println("yyyyyyyyy22:"+sql);
         for (String string : tlIgnore.get())
             if (sql.contains(string))
                 return false;
 
-        System.out.println("yyyyyyyyy33:"+sql);
         tlDataSource.get().add(dataSource);
         tlSql.get().add(sql);
         tlArgs.get().add(args);
