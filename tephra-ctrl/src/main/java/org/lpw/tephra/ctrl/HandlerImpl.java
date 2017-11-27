0d0c7886ca48caf45e8cc262821bbd193d8ceb7d
lpw <heisedebaise@hotmail.com>
2017-11-27 09:05:45 +0800

    修正请求处理队列BUG。

--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/HandlerImpl.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/HandlerImpl.java
@@ -5,6 +5,7 @@ import org.lpw.tephra.util.TimeUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -22,13 +23,15 @@ import java.util.concurrent.Future;
  */
 @Controller("tephra.ctrl.handler")
 public class HandlerImpl implements Handler, MinuteJob {
+    @Inject
+    private Counter counter;
     @Value("${tephra.ctrl.handler.queue:true}")
     private boolean queue;
     @Value("${tephra.ctrl.handler.max-idle:30}")
     private int maxIdle;
-    private Map<String, ExecutorService> executors = new ConcurrentHashMap<>();
-    private Map<String, List<Future<?>>> futures = new ConcurrentHashMap<>();
-    private Map<String, Long> times = new ConcurrentHashMap<>();
+    private Map<Integer, ExecutorService> executors = new ConcurrentHashMap<>();
+    private Map<Integer, List<Future<?>>> futures = new ConcurrentHashMap<>();
+    private Map<Integer, Long> times = new ConcurrentHashMap<>();
 
     @Override
     public <T> T call(String key, Callable<T> callable) throws Exception {
@@ -37,30 +40,36 @@ public class HandlerImpl implements Handler, MinuteJob {
 
     @Override
     public <T> Future<T> submit(String key, Callable<T> callable) {
-        Future<T> future = getExecutorService(key).submit(callable);
-        addFuture(key, future);
+        Integer nKey = getKey(key);
+        Future<T> future = getExecutorService(nKey).submit(callable);
+        addFuture(nKey, future);
 
         return future;
     }
 
     @Override
     public void run(String key, Runnable runnable) {
-        if (queue)
-            addFuture(key, getExecutorService(key).submit(runnable));
-        else
+        if (queue) {
+            Integer nKey = getKey(key);
+            addFuture(nKey, getExecutorService(nKey).submit(runnable));
+        } else
             runnable.run();
     }
 
-    private ExecutorService getExecutorService(String key) {
+    private ExecutorService getExecutorService(Integer key) {
         return executors.computeIfAbsent(key, k -> Executors.newSingleThreadExecutor());
     }
 
-    private void addFuture(String key, Future<?> future) {
+    private void addFuture(Integer key, Future<?> future) {
         futures.computeIfAbsent(key, k -> Collections.synchronizedList(new ArrayList<>())).add(future);
     }
 
     @Override
     public void clear(String key) {
+        clear(getKey(key));
+    }
+
+    private void clear(Integer key) {
         if (executors.containsKey(key))
             executors.remove(key).shutdown();
         if (futures.containsKey(key))
@@ -69,12 +78,16 @@ public class HandlerImpl implements Handler, MinuteJob {
             times.remove(key);
     }
 
+    private Integer getKey(String key) {
+        return (key.hashCode() & Integer.MAX_VALUE) % counter.max();
+    }
+
     @Override
     public void executeMinuteJob() {
         if (!queue)
             return;
 
-        Set<String> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         futures.forEach((key, list) -> {
             if (list.isEmpty()) {
                 if (System.currentTimeMillis() - times.getOrDefault(key, 0L) > maxIdle * TimeUnit.Minute.getTime())
