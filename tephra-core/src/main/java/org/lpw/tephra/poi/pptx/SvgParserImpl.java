ccf179bfb097da819a62109e2bfe0342525ef809
lpw <heisedebaise@hotmail.com>
2018-02-27 17:42:17 +0800

    完善PPTx导出功能。

--- tephra-core/src/main/java/org/lpw/tephra/poi/pptx/SvgParserImpl.java
+++ tephra-core/src/main/java/org/lpw/tephra/poi/pptx/SvgParserImpl.java
@@ -12,8 +12,10 @@ import org.apache.poi.xslf.usermodel.XSLFPictureShape;
 import org.apache.poi.xslf.usermodel.XSLFShape;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import org.lpw.tephra.poi.StreamWriter;
+import org.lpw.tephra.util.Converter;
 import org.lpw.tephra.util.Http;
 import org.lpw.tephra.util.Logger;
+import org.lpw.tephra.util.Numeric;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -21,6 +23,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author lpw
@@ -30,9 +34,14 @@ public class SvgParserImpl implements Parser {
     @Inject
     private Http http;
     @Inject
+    private Converter converter;
+    @Inject
+    private Numeric numeric;
+    @Inject
     private Logger logger;
     @Inject
     private ParserHelper parserHelper;
+    private Pattern pattern = Pattern.compile("viewBox=\"[^\"]+\"");
 
     @Override
     public String getType() {
@@ -58,13 +67,36 @@ public class SvgParserImpl implements Parser {
 
     private ByteArrayOutputStream readSvg(String image) throws IOException, TranscoderException {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        Reader reader = new StringReader(image);
+        Reader reader = new StringReader(fixViewBox(image));
         new PNGTranscoder().transcode(new TranscoderInput(reader), new TranscoderOutput(outputStream));
         reader.close();
         outputStream.flush();
         outputStream.close();
 
         return outputStream;
+    }
+
+    private String fixViewBox(String image) {
+        Matcher matcher = pattern.matcher(image);
+        if (!matcher.find())
+            return image;
+
+        String viewBox = matcher.group();
+        String[] array = converter.toArray(viewBox.substring(9, viewBox.length() - 1), " ");
+        double[] ns = new double[]{numeric.toDouble(array[2]), numeric.toDouble(array[3])};
+        if (ns[0] <= 2048 && ns[1] <= 2048)
+            return image;
+
+        if (ns[0] > 2048) {
+            ns[1] = ns[1] * 2048 / ns[0];
+            ns[0] = 2048;
+        }
+        if (ns[1] > 2048) {
+            ns[0] = ns[0] * 2048 / ns[1];
+            ns[1] = 2048;
+        }
+
+        return matcher.replaceFirst("viewBox=\"0 0 " + ns[0] + " " + ns[1] + "\"");
     }
 
     @Override
