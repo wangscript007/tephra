82e9b297242c5bf5a593cf5f15c34fb33b32d30a
lpw <heisedebaise@hotmail.com>
2018-03-15 18:30:08 +0800

    完善验证器。

--- tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/SignValidatorImpl.java
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/SignValidatorImpl.java
@@ -1,47 +1,10 @@
 package org.lpw.tephra.ctrl.validate;
 
-import org.lpw.tephra.crypto.Sign;
-import org.lpw.tephra.ctrl.context.Header;
-import org.lpw.tephra.ctrl.security.TrustfulIp;
 import org.springframework.stereotype.Controller;
-
-import javax.inject.Inject;
 
 /**
  * @author lpw
  */
 @Controller(Validators.SIGN)
-public class SignValidatorImpl extends ValidatorSupport implements SignValidator {
-    @Inject
-    private Sign sign;
-    @Inject
-    private Header header;
-    @Inject
-    private TrustfulIp trustfulIp;
-    private ThreadLocal<Boolean> threadLocal = new ThreadLocal<>();
-
-    @Override
-    public void setSignEnable(boolean enable) {
-        threadLocal.set(enable);
-    }
-
-    @Override
-    public boolean validate(ValidateWrapper validate, String parameter) {
-        return !enable() || trustfulIp.contains(header.getIp())
-                || sign.verify(request.getMap(), validator.isEmpty(validate.getString()) ? null : validate.getString()[0]);
-    }
-
-    private boolean enable() {
-        return threadLocal.get() == null || threadLocal.get();
-    }
-
-    @Override
-    public int getFailureCode(ValidateWrapper validate) {
-        return 9995;
-    }
-
-    @Override
-    protected String getDefaultFailureMessageKey() {
-        return Validators.PREFIX + "illegal-sign";
-    }
+public class SignValidatorImpl extends SignValidatorSupport {
 }
---
+++ tephra-ctrl/src/main/java/org/lpw/tephra/ctrl/validate/SignValidatorSupport.java
@@ -0,0 +1,45 @@
+package org.lpw.tephra.ctrl.validate;
+
+import org.lpw.tephra.crypto.Sign;
+import org.lpw.tephra.ctrl.context.Header;
+import org.lpw.tephra.ctrl.security.TrustfulIp;
+
+import javax.inject.Inject;
+
+/**
+ * @author lpw
+ */
+public class SignValidatorSupport extends ValidatorSupport implements SignValidator {
+    @Inject
+    private Sign sign;
+    @Inject
+    private Header header;
+    @Inject
+    private TrustfulIp trustfulIp;
+    private ThreadLocal<Boolean> threadLocal = new ThreadLocal<>();
+
+    @Override
+    public void setSignEnable(boolean enable) {
+        threadLocal.set(enable);
+    }
+
+    @Override
+    public boolean validate(ValidateWrapper validate, String parameter) {
+        return !enable() || trustfulIp.contains(header.getIp())
+                || sign.verify(request.getMap(), validator.isEmpty(validate.getString()) ? null : validate.getString()[0]);
+    }
+
+    private boolean enable() {
+        return threadLocal.get() == null || threadLocal.get();
+    }
+
+    @Override
+    public int getFailureCode(ValidateWrapper validate) {
+        return 9995;
+    }
+
+    @Override
+    protected String getDefaultFailureMessageKey() {
+        return Validators.PREFIX + "illegal-sign";
+    }
+}
