66d5264f7344e8a064bc3cc4c14b201c5f576e85
lpw <heisedebaise@hotmail.com>
2018-05-15 10:41:45 +0800

    完善Office pptx解析功能。

--- tephra-office/src/main/java/org/lpw/tephra/office/pptx/parser/GeometryParserImpl.java
+++ tephra-office/src/main/java/org/lpw/tephra/office/pptx/parser/GeometryParserImpl.java
@@ -40,8 +40,6 @@ public class GeometryParserImpl implements Parser {
 
         geometry.put("type", xslfSimpleShape.getShapeType() == null ? "rect" : format(xslfSimpleShape.getShapeType().toString()));
         shape.put("geometry", geometry);
-        System.out.println("###############################");
-        System.out.println(xslfSimpleShape.getXmlObject());
     }
 
     private void parseLine(XSLFSimpleShape xslfSimpleShape, JSONObject geometry) {
