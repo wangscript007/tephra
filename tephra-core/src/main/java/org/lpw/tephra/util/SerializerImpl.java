26c320b9bc419b60f6bf17d5c02a729db7137919
lpw <lpw@nutspie.com>
2018-12-28 11:11:13 +0800

    完善序列化工具。

--- tephra-core/src/main/java/org/lpw/tephra/util/SerializerImpl.java
+++ tephra-core/src/main/java/org/lpw/tephra/util/SerializerImpl.java
@@ -1,16 +1,14 @@
 package org.lpw.tephra.util;
 
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
 /**
@@ -21,13 +19,7 @@ public class SerializerImpl implements Serializer {
     @Inject
     private Validator validator;
     @Inject
-    private Json json;
-    @Inject
-    private Io io;
-    @Inject
     private Logger logger;
-    private byte[] jsonObject = "json->object::".getBytes();
-    private byte[] jsonArray = "json->array::".getBytes();
 
     @Override
     public byte[] serialize(Object object) {
@@ -45,30 +37,12 @@ public class SerializerImpl implements Serializer {
         if (object == null || outputStream == null)
             return;
 
-        if (object instanceof JSONObject) {
-            serializeJson(jsonObject, object, outputStream);
-
-            return;
-        }
-
-        if (object instanceof JSONArray) {
-            serializeJson(jsonArray, object, outputStream);
-
-            return;
-        }
-
-        Output output = new Output(outputStream);
-        kryo().writeClassAndObject(output, object);
-        output.close();
-    }
-
-    private void serializeJson(byte[] bytes, Object object, OutputStream outputStream) {
         try {
-            outputStream.write(bytes);
-            outputStream.write(json.toBytes(object));
-            outputStream.close();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(object);
+            objectOutputStream.close();
         } catch (IOException e) {
-            logger.warn(e, "序列化JSON数据[{}]时发生异常！", object);
+            logger.warn(e, "序列化[{}]时发生异常！", object);
         }
     }
 
@@ -77,46 +51,25 @@ public class SerializerImpl implements Serializer {
         if (validator.isEmpty(bytes))
             return null;
 
-        T object = unserializeJson(bytes);
-
-        return object == null ? unserializeByKryo(bytes) : object;
+        return unserialize(new ByteArrayInputStream(bytes));
     }
 
     @Override
+    @SuppressWarnings({"unchecked"})
     public <T> T unserialize(InputStream inputStream) {
         if (inputStream == null)
             return null;
 
-        return unserialize(io.read(inputStream));
-    }
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+            T object = (T) objectInputStream.readObject();
+            objectInputStream.close();
 
-    @SuppressWarnings("unchecked")
-    private <T> T unserializeJson(byte[] bytes) {
-        if (bytes.length <= jsonArray.length)
+            return object;
+        } catch (Exception e) {
+            logger.warn(e, "反序列化时发生异常！");
+
             return null;
-
-        if (validator.startsWith(bytes, jsonObject))
-            return (T) json.toObject(new String(bytes).substring(jsonObject.length), false);
-
-        if (validator.startsWith(bytes, jsonArray))
-            return (T) json.toArray(new String(bytes).substring(jsonArray.length), false);
-
-        return null;
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T unserializeByKryo(byte[] bytes) {
-        Input input = new Input(bytes);
-        T object = (T) kryo().readClassAndObject(input);
-        input.close();
-
-        return object;
-    }
-
-    private Kryo kryo() {
-        Kryo kryo = new Kryo();
-        kryo.setRegistrationRequired(false);
-
-        return kryo;
+        }
     }
 }
