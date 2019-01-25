b231d932a7241d9e2f1bdeba1cb3153c9a81097d
heisedebaise <heisedebaise@hotmail.com>
2019-01-25 11:09:56 +0800

    完善PDF功能。

--- tephra-pdf/src/main/java/org/lpw/tephra/pdf/PdfReader.java
+++ tephra-pdf/src/main/java/org/lpw/tephra/pdf/PdfReader.java
@@ -3,6 +3,7 @@ package org.lpw.tephra.pdf;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * PDF读取器。
@@ -18,4 +19,23 @@ public interface PdfReader {
      * @return JSON数据。
      */
     JSONObject read(InputStream inputStream, MediaWriter mediaWriter);
+
+    /**
+     * 转化为图片。
+     *
+     * @param inputStream 输入流。
+     * @param mediaWriter 媒体输出器。
+     * @param page        转化页码，0表示第一页。
+     * @return 图片URL地址。
+     */
+    String readAsJpeg(InputStream inputStream, MediaWriter mediaWriter, int page);
+
+    /**
+     * 转化为图片。
+     *
+     * @param inputStream 输入流。
+     * @param mediaWriter 媒体输出器。
+     * @return 图片URL地址集。
+     */
+    List<String> readAsJpeg(InputStream inputStream, MediaWriter mediaWriter);
 }
--- tephra-pdf/src/main/java/org/lpw/tephra/pdf/PdfReaderImpl.java
+++ tephra-pdf/src/main/java/org/lpw/tephra/pdf/PdfReaderImpl.java
@@ -4,14 +4,22 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.lpw.tephra.pdf.parser.ImageParser;
 import org.lpw.tephra.pdf.parser.TextParser;
 import org.lpw.tephra.util.Logger;
 import org.springframework.stereotype.Component;
 
+import javax.imageio.ImageIO;
 import javax.inject.Inject;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author lpw
@@ -82,5 +90,56 @@ public class PdfReaderImpl implements PdfReader {
     private void merge(JSONArray elements, JSONArray array) {
         if (!array.isEmpty())
             elements.addAll(array);
+    }
+
+    @Override
+    public String readAsJpeg(InputStream inputStream, MediaWriter mediaWriter, int page) {
+        String url = null;
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); PDDocument document = PDDocument.load(inputStream)) {
+            ImageIO.write(new PDFRenderer(document).renderImage(0, 1.0f, ImageType.RGB), "JPEG", byteArrayOutputStream);
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            url = mediaWriter.write(MediaType.Jpeg, page + ".jpeg", byteArrayInputStream);
+            byteArrayInputStream.close();
+        } catch (IOException e) {
+            logger.warn(e, "读取PDF为图片时发生异常！");
+        }
+
+        return url;
+    }
+
+    @Override
+    public List<String> readAsJpeg(InputStream inputStream, MediaWriter mediaWriter) {
+        List<String> list = new ArrayList<>();
+        try (PDDocument document = PDDocument.load(inputStream)) {
+            PDFRenderer renderer = new PDFRenderer(document);
+            BufferedImage together = null;
+            for (int i = 0, size = document.getNumberOfPages(); i < size; i++) {
+                BufferedImage bufferedImage = renderer.renderImage(i, 1.0f, ImageType.RGB);
+                list.add(write(mediaWriter, bufferedImage, i + ".jpeg"));
+
+                if (together == null)
+                    together = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight() * size, BufferedImage.TYPE_INT_RGB);
+                together.getGraphics().drawImage(bufferedImage, 0, i * bufferedImage.getHeight(), null);
+            }
+
+            if (together != null)
+                list.add(write(mediaWriter, together, "together.jpeg"));
+        } catch (IOException e) {
+            logger.warn(e, "读取PDF为图片时发生异常！");
+        }
+
+        return list;
+    }
+
+    private String write(MediaWriter mediaWriter, BufferedImage bufferedImage, String fileName) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "JPEG", outputStream);
+        outputStream.close();
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        String url = mediaWriter.write(MediaType.Jpeg, fileName, inputStream);
+        inputStream.close();
+
+        return url;
     }
 }
