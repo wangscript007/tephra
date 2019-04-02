5f91698d3197cc53ad386864081e26e3aa56bf2c
heisedebaise <heisedebaise@hotmail.com>
2019-04-02 23:33:00 +0800

    添加SSH客户端工具。

---
+++ tephra-core/src/main/java/org/lpw/tephra/util/Ssh.java
@@ -0,0 +1,50 @@
+package org.lpw.tephra.util;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * SSH客户端。
+ *
+ * @author lpw
+ */
+public interface Ssh {
+    /**
+     * 执行指令集。
+     *
+     * @param host     主机。
+     * @param port     端口号。
+     * @param user     用户。
+     * @param password 密码。
+     * @param commands 指令集。
+     * @return 执行结果；如果执行失败则返回null。
+     */
+    String shell(String host, int port, String user, String password, String... commands);
+
+    /**
+     * 下载文件。
+     *
+     * @param host         主机。
+     * @param port         端口号。
+     * @param user         用户。
+     * @param password     密码。
+     * @param path         文件路径。
+     * @param outputStream 输出流。
+     * @return 获取成功则返回true；否则返回false。
+     */
+    boolean get(String host, int port, String user, String password, String path, OutputStream outputStream);
+
+    /**
+     * 上传文件。
+     *
+     * @param host        主机。
+     * @param port        端口号。
+     * @param user        用户。
+     * @param password    密码。
+     * @param path        文件路径。
+     * @param mode        文件模式。
+     * @param inputStream 输入流。
+     * @return 获取成功则返回true；否则返回false。
+     */
+    boolean put(String host, int port, String user, String password, String path, int mode, InputStream inputStream);
+}
---
+++ tephra-core/src/main/java/org/lpw/tephra/util/SshImpl.java
@@ -0,0 +1,110 @@
+package org.lpw.tephra.util;
+
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelShell;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+/**
+ * @author lpw
+ */
+@Component("tephra.util.ssh")
+public class SshImpl implements Ssh {
+    @Inject
+    private Validator validator;
+    @Inject
+    private Io io;
+    @Inject
+    private Logger logger;
+
+    @Override
+    public String shell(String host, int port, String user, String password, String... commands) {
+        if (validator.isEmpty(host) || port < 1 || validator.isEmpty(user) || validator.isEmpty(commands))
+            return null;
+
+        try {
+            Session session = getSession(host, port, user, password);
+            ChannelShell channelShell = (ChannelShell) session.openChannel("shell");
+            InputStream inputStream = channelShell.getInputStream();
+            PrintWriter writer = new PrintWriter(channelShell.getOutputStream());
+            channelShell.connect();
+            for (String command : commands)
+                writer.println(command);
+            writer.println("exit");
+            writer.flush();
+            String string = null;
+            if (inputStream.available() > 0)
+                string = io.readAsString(inputStream);
+            writer.close();
+            inputStream.close();
+            session.disconnect();
+
+            return string;
+        } catch (Throwable throwable) {
+            logger.warn(throwable, "执行SSH[{}:{}:{}:{}:{}]时发生异常！", host, port, user, password, Arrays.toString(commands));
+
+            return null;
+        }
+    }
+
+    @Override
+    public boolean get(String host, int port, String user, String password, String path, OutputStream outputStream) {
+        if (validator.isEmpty(host) || port < 1 || validator.isEmpty(user) || validator.isEmpty(path))
+            return false;
+
+        try {
+            Session session = getSession(host, port, user, password);
+            ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
+            channelSftp.connect();
+            channelSftp.get(path, outputStream);
+            outputStream.close();
+            channelSftp.disconnect();
+            session.disconnect();
+
+            return true;
+        } catch (Throwable throwable) {
+            logger.warn(throwable, "下载[{}:{}:{}:{}]文件[{}]时发生异常！", host, port, user, password, path);
+
+            return false;
+        }
+    }
+
+    @Override
+    public boolean put(String host, int port, String user, String password, String path, int mode, InputStream inputStream) {
+        if (validator.isEmpty(host) || port < 1 || validator.isEmpty(user) || validator.isEmpty(path))
+            return false;
+
+        try {
+            Session session = getSession(host, port, user, password);
+            ChannelSftp channelSftp = (ChannelSftp) session.openChannel("sftp");
+            channelSftp.connect();
+            channelSftp.put(inputStream, path, mode);
+            inputStream.close();
+            channelSftp.disconnect();
+            session.disconnect();
+
+            return true;
+        } catch (Throwable throwable) {
+            logger.warn(throwable, "上传[{}:{}:{}:{}]文件[{}]时发生异常！", host, port, user, password, path);
+
+            return false;
+        }
+    }
+
+    private Session getSession(String host, int port, String user, String password) throws JSchException {
+        Session session = new JSch().getSession(user, host, port);
+        session.setPassword(password);
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect(60 * 60 * 1000);
+
+        return session;
+    }
+}
